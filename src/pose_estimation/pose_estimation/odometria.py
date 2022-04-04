a7d7375b8144a4f3663aacf79906401745e3c78b
LuanAmaral <luan.rocha.amaral@gmail.com>
2022-04-04 16:07:14 -0300

    corrigido bugs da odometria

--- src/pose_estimation/pose_estimation/odometria.py
+++ src/pose_estimation/pose_estimation/odometria.py
@@ -1,3 +1,4 @@
+from re import I
 import cv2
 import numpy as np
 from .camera_params import *
@@ -5,10 +6,16 @@ import os
 import argparse
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
 
 #Defines
 FRAME_RATE = 20
+    # params for corner detection
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
     # Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (15, 15),
                   maxLevel = 2,
@@ -57,31 +64,36 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('Visual_Odometry')
-        self.publisher_ = self.create_publisher(PoseStamped, 'Pose', 10)
+        self.publisher_ = self.create_publisher(PoseArray, 'Pose', 10)
         timer_period = 1/FRAME_RATE  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.R = np.zeros([3,3])
-        self.t = np.zeros(3)
+        self.R = np.identity(3)
+        self.t = np.array([[0],[0],[0]])
         self.q = np.zeros(4)
-        self.pose = PoseStamped()
-        self.pose.header.frame_id = "VisOdometry"
+        self.pose = PoseArray()
+        self.pose.header.frame_id = "map"
         if cv2.getWindowProperty("odometry", 0) >= 0:
             ret, imgk_1 = cap.read()
             self.grayk_1 = cv2.cvtColor(imgk_1, cv2.COLOR_BGR2GRAY)
             self.grayk_1 = cv2.GaussianBlur(self.grayk_1, (5,5), 0)
-            self.kpk_1 = fast.detect(self.grayk_1)
+            # self.kpk_1 = fast.detect(self.grayk_1)
+            self.kpk_1 = cv2.goodFeaturesToTrack(self.grayk_1, mask = None,  **feature_params)
 
     def timer_callback(self):
         self.odometria()
-        self.pose.pose.position.x = self.t[0]
-        self.pose.pose.position.y = self.t[1]
-        self.pose.pose.position.z = self.t[2] 
-        self.pose.pose.orientation.w = float(1)
-        self.pose.pose.orientation.x = self.q[1]
-        self.pose.pose.orientation.y = self.q[2]
-        self.pose.pose.orientation.z = self.q[3]
+
+        pose = Pose()
+        pose.position.x = float(self.t[0])
+        pose.position.y = float(self.t[1])
+        pose.position.z = float(self.t[2]) 
+        pose.orientation.w = float(self.q[0])
+        pose.orientation.x = float(self.q[1])
+        pose.orientation.y = float(self.q[2])
+        pose.orientation.z = float(self.q[3])
+
         self.pose.header.stamp= self.get_clock().now().to_msg()
+        self.pose.poses.append(pose)
 
         self.publisher_.publish(self.pose)
         # self.get_logger().info('Number of points: "%d"', self.kpk)
@@ -94,19 +106,21 @@ class MinimalPublisher(Node):
             grayk = cv2.GaussianBlur(grayk, (5,5), 0)
             # kpk_1 = fast.detect(grayk_1)
             # kpk = fast.detect(grayk)
+        
+            kpk, st, err = cv2.calcOpticalFlowPyrLK(self.grayk_1, grayk, self.kpk_1, None, **lk_params)
+            E, mask = cv2.findEssentialMat(self.kpk_1, kpk, K, cv2.FM_RANSAC, 0.90, 1)
+            ret, R_, t_, mask = cv2.recoverPose(E, self.kpk_1, kpk, K)
+            
+            self.t = self.R.dot(t_) + self.t
+            self.R = self.R.dot(R_)
+            self.q = convert_matrix2quaternions(self.R)
 
-            # kpk, st, err = cv2.calcOpticalFlowPyrLK(np.float32(self.grayk_1), grayk, self.kpk_1, None, **lk_params)
-            # E, mask = cv2.findEssentialMat(self.kpk_1, kpk, K, cv2.FM_RANSAC, 0.90, 1)
-            # ret, R_, t_, mask = cv2.recoverPose(E, self.kpk_1, kpk, K)
-
-            # self.t = self.R*t_ + self.t
-            # self.R = self.R.dot(R_)
-            # self.q = convert_matrix2quaternions(self.R)
-
-            imgk = cv2.drawKeypoints(imgk, self.kpk_1, None, color=(255,0,0))
+            # imgk = cv2.drawKeypoints(imgk, np.float32(self.kpk_1), None, color=(255,0,0))
             cv2.imshow('odometry', imgk)
 
-            self.kpk_1 = fast.detect(grayk)
+            # self.kpk_1 = fast.detect(grayk)
+            self.kpk_1 = cv2.goodFeaturesToTrack(grayk, mask = None,  **feature_params)
+            self.grayk_1 = grayk
 
             keyCode = cv2.waitKey(30) & 0xFF
             if keyCode == 27:
